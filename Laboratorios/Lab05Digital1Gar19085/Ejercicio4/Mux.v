ede334311925ff1adf9d65176a9a0af12aa7eecc
gar19085 <68084823+gar19085@users.noreply.github.com>
2020-08-14 22:26:08 -0600

    Lab5 V1.0

---
+++ Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux.v
@@ -0,0 +1,17 @@
+//Universidad del Valle de Guatemala
+//Digital 1
+//Laboratorio 05
+//Rodrigo José García Ambrosy. Carnet: 19085
+
+//Implemento mmódulo Mux 2:1 
+module Mux2x1(input wire A, B, SEL, Gbar, output wire Y);
+//ands
+wire a1, a2;
+//nots
+wire n1;
+
+//operadores del mux2:1
+not(n1, );
+and(a1, );
+and(a2, );
+or(Y, );
