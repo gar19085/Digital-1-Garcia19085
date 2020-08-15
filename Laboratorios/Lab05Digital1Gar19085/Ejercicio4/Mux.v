ce3bce7e60250ae38883a3cea2252dbbaf1e36d0
gar19085 <68084823+gar19085@users.noreply.github.com>
2020-08-15 13:45:43 -0600

    V2.1
    Prueba creación de tablas

--- Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux.v
+++ Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux.v
@@ -31,17 +31,56 @@ module Mux8x1(input wire [3:0] D0, D1, D2, D3, D4, D5, D6, D7, D8,
               input wire [1:0] S2,
               output wire [3:0] Y);
 
-        wire [3:0] SA, SB, SC;
+        wire [3:0] SA, SB;
     
-    Mux4x1 SA(D0, D1, D2, D3. S2[0], SA);
-    Mux4x1 SB(D4, D5, D6, D7, S2[0], SB); 
-    Mux2X1 SC(SA, SB, S2[0], SC);
-    Mux2x1 OUT(SC, S2[1], Y);
+    Mux4x1 lomux(D0, D1, D2, D3, S2[0], SA);
+    Mux4x1 himux(D4, D5, D6, D7, S2[0], SB); 
+    Mux2x1 finalmux(SA, SB, S2[0], Y);
+    
 
 endmodule //CIERRO MODULO
 
 
+//IMPLEMENTO TABLA1 DEL MUX2:1
+    module M2x1Tabla01( input wire [1:0] B, C,
+                        input wire   A, 
+                        output wire [1:0] Y);
+
+        Mux2x1  M1(A, B, C, Y);
+
+    endmodule 
 
 
+//IMPLEMENTO TABLA1 DEL MUX4:1
+    module M4x1Tabla01();
 
+    endmodule
+
+
+//IMPLEMENTO TABLA1 DEL MUX8:1
+    module M8x1Tabla01();
+
+    endmodule
+
+
+//IMPLEMENTO TABLA 2 DEL MUX2:1
+    module M2x1Tabla02( input wire [1:0] B, C,
+                        input wire   A, 
+                        output wire [1:0] Y);
+
+        Mux2x1  M1(A, B, C, Y);
+
+    endmodule 
+
+
+//IMPLEMENTO TABLA2 DEL MUX4:1
+    module M4x1Tabla02();
+
+    endmodule
+
+
+//IMPLEMENTO TABLA2 DEL MUX8:1
+    module M8x1Tabla02();
+
+    endmodule
 
--- Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux_tb.v
+++ Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux_tb.v
@@ -6,28 +6,28 @@
 
 module  TESTBENCH();
     //MUX 2:1
-    reg D0, D1, S; //TABLA 01
-    wire Y;
+    reg t1, t2, t3; //TABLA 01
+    wire PzI;
 
     //MUX 4:1
 
 //MUX 2:1
-    Mux2x1 M2(D0, D1, S, Y);
+    M2x1Tabla01 M2(t1, t2, t3, PzI);
 
 
     initial begin
         $display("MUX2:1");
-        $display("S D1 D0 | Y");
-        $display("--------|---");
-        $monitor("%b %b %b | %b", S, D1, D0, Y);
-           S = 0; D1 = 0; D0 = 0;
-        #1 S = 0; D1 = 0; D0 = 1;
-        #1 S = 0; D1 = 1; D0 = 0;
-        #1 S = 0; D1 = 1; D0 = 1;
-        #1 S = 1; D1 = 0; D0 = 0;
-        #1 S = 1; D1 = 0; D0 = 1;
-        #1 S = 1; D1 = 1; D0 = 0;
-        #1 S = 1; D1 = 1; D0 = 1;
+        $display("A B C | Y");
+        $display("------|---");
+        $monitor("%b %b %b | %b", t1, t2, t3, PzI);
+           t1 = 0; t2 = 0; t3 = 0;
+        #1 t1 = 0; t2 = 0; t3 = 1;
+        #1 t1 = 0; t2 = 1; t3 = 0;
+        #1 t1 = 0; t2 = 1; t3 = 1;
+        #1 t1 = 1; t2 = 0; t3 = 0;
+        #1 t1 = 1; t2 = 0; t3 = 1;
+        #1 t1 = 1; t2 = 1; t3 = 0;
+        #1 t1 = 1; t2 = 1; t3 = 1;
     end //Termina en 8
 
 
