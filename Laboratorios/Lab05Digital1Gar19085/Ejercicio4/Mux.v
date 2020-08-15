1250b109fdb212a6de6622fae5a09a3ac7ea62cb
gar19085 <68084823+gar19085@users.noreply.github.com>
2020-08-15 00:30:28 -0600

    V1.2
    Mux 4:1 pruebas

--- Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux.v
+++ Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux.v
@@ -13,4 +13,14 @@ module Mux2x1(input wire [3:0] D0, D1,
 endmodule
 //CIERRO EL MODULO DE MUX2:1 PARA PODER IMPLEMENTARLO EN EL MUX4:1
 
+//IMPLEMENTACIÓN DE MUX4:1 CON MUX2:1
+module Mux4x1(input wire [3:0]  D0, D1, D2, D3,
+              input wire [1:0]  S,
+              output wire [3:0] Y);
+    wire [3:0] low, high;
 
+    Mux2x1 lomux(D0, D1, S[0], low);
+    Mux2x1 himux(D2, D3, S[0], high);
+    Mux2x1 outmux(low, high, S[1], Y);
+
+endmodule
