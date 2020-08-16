c626d0ce0413a65f27611a67b7f3d75584e20010
gar19085 <68084823+gar19085@users.noreply.github.com>
2020-08-15 21:49:28 -0600

    V.3.
    Correciones

--- Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux.v
+++ Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux.v
@@ -4,83 +4,114 @@
 //Rodrigo José García Ambrosy. Carnet: 19085
 
 //IMPLEMENTO DEL MODULO MUX2:1 UTILIZADO EL OPERADOR TERNARIO
-module Mux2x1(input wire [3:0] D0, D1, 
+module Mux2x1(input wire  D0, D1, 
               input wire      S,
-              output wire [3:0] Y);
+              output wire  Y);
     
     assign Y = S ? D1 : D0;
 
 endmodule //CIERRO MODULO
 
 //IMPLEMENTACIÓN DE MUX4:1 CON MUX2:1
-module Mux4x1(input wire [3:0]  D0, D1, D2, D3,
-              input wire [1:0]  S1,
-              output wire [3:0] Y);
+module Mux4x1(input wire   D0, D1, D2, D3,
+              input wire   S0, S1,
+              output wire  Y);
 
-    wire [3:0] low, hig;
+    wire  low, hig;
 
-    Mux2x1 lowmux(D0, D1, S1[0], low);
-    Mux2x1 higmux(D2, D3, S1[0], high);
-    Mux2x1 outmux(low, high, S1[1], Y);
+    Mux2x1 lowmux(D0, D1, S0, low);
+    Mux2x1 higmux(D2, D3, S0, high);
+    Mux2x1 outmux(low, high, S1, Y);
 
 endmodule //CIERRO MODULO
 
 
 //IMPLEMENTACIÓN DE MUX8:1 CON MUX4:1
-module Mux8x1(input wire [3:0] D0, D1, D2, D3, D4, D5, D6, D7, D8,
-              input wire [1:0] S2,
-              output wire [3:0] Y);
+module Mux8x1(input wire  D0, D1, D2, D3, D4, D5, D6, D7,
+              input wire  S0, S1, S2, 
+              output wire  Y);
 
-        wire [3:0] SA, SB;
+        wire  SA, SB;
     
-    Mux4x1 lomux(D0, D1, D2, D3, S2[0], SA);
-    Mux4x1 himux(D4, D5, D6, D7, S2[0], SB); 
-    Mux2x1 finalmux(SA, SB, S2[0], Y);
+    Mux4x1 Tig1(D0, D1, D2, D3, S0, S1, SA);
+    Mux4x1 Tig2(D4, D5, D6, D7, S0, S1, SB); 
+    Mux2x1 Tig3(SA, SB, S2, Y);
     
 
 endmodule //CIERRO MODULO
 
 
 //IMPLEMENTO TABLA1 DEL MUX2:1
-    module M2x1Tabla01( input wire [1:0] B, C,
-                        input wire   A, 
-                        output wire [1:0] Y);
+    module M2x1Tabla01( input wire  A, B, C,
+                        output wire Y);
 
-        Mux2x1  M1(A, B, C, Y);
+        wire O1, O2; //Van en el Mux
+        wire R1, R2;
+
+        //compuertas
+            xor  (R1, B, C);
+            xnor (R2, B, C);
+
+        assign O1 = R1;
+        assign O2 = R2;
+
+        Mux2x1  M1(O1, O2, A, Y);
 
     endmodule 
 
 
 //IMPLEMENTO TABLA1 DEL MUX4:1
-    module M4x1Tabla01();
+    module M4x1Tabla01( input wire C,
+                        input wire S, S1,
+                        output wire Y);
+        wire N1, N2; //Va al mux
+        wire No1;
+
+        //compuertas
+            not (No1, C);
+
+        assign N1 = No1;
+        assign N2 = No1;
+    
+
+        Mux4x1 M2(C, N1, N2, C, S, S1, Y);  
+        
 
     endmodule
 
 
 //IMPLEMENTO TABLA1 DEL MUX8:1
-    module M8x1Tabla01();
+    module M8x1Tabla01(input wire A, B, C,
+                        output wire Y);
+        wire V, G;
+
+        assign V = 1;
+        assign G = 0;
+       
+
+    Mux8x1 M3(G, V, V, G, V, G, G, V, A, B, C, Y);
 
     endmodule
 
 
 //IMPLEMENTO TABLA 2 DEL MUX2:1
-    module M2x1Tabla02( input wire [1:0] B, C,
-                        input wire   A, 
-                        output wire [1:0] Y);
+  //  module M2x1Tabla02( input wire [1:0] B, C,
+  //                      input wire   A, 
+  //                      output wire [1:0] Y);
 
-        Mux2x1  M1(A, B, C, Y);
+//        Mux2x1  M1(A, B, C, Y);
 
-    endmodule 
+//    endmodule 
 
 
 //IMPLEMENTO TABLA2 DEL MUX4:1
-    module M4x1Tabla02();
+//    module M4x1Tabla02();
 
-    endmodule
+//    endmodule
 
 
 //IMPLEMENTO TABLA2 DEL MUX8:1
-    module M8x1Tabla02();
+//    module M8x1Tabla02();
 
-    endmodule
+//    endmodule
 
--- Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux_tb.v
+++ Laboratorios/Lab05Digital1Gar19085/Ejercicio4/Mux_tb.v
@@ -5,21 +5,24 @@
 //CONFIG DEL TESTBENCH
 
 module  TESTBENCH();
-    //MUX 2:1
-    reg t1, t2, t3; //TABLA 01
-    wire PzI;
+//Tabla 01
+    
+    reg t1, t2, t3, //MUX 2:1
+        t4, t5, t6, //MUX 4:1
+        t7, t8, t9; //MUX 8:1
+    wire Pz1, Pz2, Pz3;
 
-    //MUX 4:1
 
 //MUX 2:1
-    M2x1Tabla01 M2(t1, t2, t3, PzI);
+    M2x1Tabla01 M2(t1, t2, t3, Pz1);
+    M4x1Tabla01 M(t4, t5, t6, Pz2);
 
 
     initial begin
-        $display("MUX2:1");
+        $display("MUX2:1, Tabla 01");
         $display("A B C | Y");
         $display("------|---");
-        $monitor("%b %b %b | %b", t1, t2, t3, PzI);
+        $monitor("%b %b %b | %b", t1, t2, t3, Pz1);
            t1 = 0; t2 = 0; t3 = 0;
         #1 t1 = 0; t2 = 0; t3 = 1;
         #1 t1 = 0; t2 = 1; t3 = 0;
@@ -31,9 +34,45 @@ module  TESTBENCH();
     end //Termina en 8
 
 
+    initial begin
+        #9
+        $display("\n");
+        $display("MUX4:1, Tabla 01");
+        $display("A B C | Y");
+        $display("------|---");
+        $monitor("%b %b %b | %b", t4, t5, t6, Pz2);
+           t4 = 0; t5 = 0; t6 = 0;
+        #1 t4 = 0; t5 = 0; t6 = 1;
+        #1 t4 = 0; t5 = 1; t6 = 0;
+        #1 t4 = 0; t5 = 1; t6 = 1;
+        #1 t4 = 1; t5 = 0; t6 = 0;
+        #1 t4 = 1; t5 = 0; t6 = 1;
+        #1 t4 = 1; t5 = 1; t6 = 0;
+        #1 t4 = 1; t5 = 1; t6 = 1;
+    end //Termina en 16
+
+    initial begin
+        #17
+        $display("\n");
+        $display("MUX8:1, Tabla 01");
+        $display("A B C | Y");
+        $display("------|---");
+        $monitor("%b %b %b | %b", t7, t8, t9, Pz3);
+           t7 = 0; t8 = 0; t9 = 0;
+        #1 t7 = 0; t8 = 0; t9 = 1;
+        #1 t7 = 0; t8 = 1; t9 = 0;
+        #1 t7 = 0; t8 = 1; t9 = 1;
+        #1 t7 = 1; t8 = 0; t9 = 0;
+        #1 t7 = 1; t8 = 0; t9 = 1;
+        #1 t7 = 1; t8 = 1; t9 = 0;
+        #1 t7 = 1; t8 = 1; t9 = 1;
+    end //Termina en  24
+
+
+
 //TESTBENCH MUX2:1
     initial 
-        #9 $finish;
+        #25 $finish;
 
     initial begin  
         $dumpfile("Mux_tb.vcd");
