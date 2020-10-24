2cd4c765b22ca9effe44f0784d33c3ad3bebc1f0
gar19085 <68084823+gar19085@users.noreply.github.com>
2020-10-23 22:41:09 -0600

    Lab08 V2.5
    ROM finalizada

--- Laboratorios/Lab08Digital1Gar19085/Ejercicio02/ROM.v
+++ Laboratorios/Lab08Digital1Gar19085/Ejercicio02/ROM.v
@@ -2,13 +2,15 @@
 //LAB08
 
 
-module  ROM4Kx8(input   [7:0]address
-                output  [7:0]data_out);
-
-    assign data_out = my_memory(address);
+module  ROM4Kx8(address, data_out);
+    input   [7:0] address;
+    output  [7:0] data_out;
+    
+    assign data_out = my_memory[address];
     reg [7:0]   my_memory[0:4095];
 
     initial begin
         $readmemh("memory.list", my_memory);
     end
+
 endmodule    
--- Laboratorios/Lab08Digital1Gar19085/Ejercicio02/ROM_tb.v
+++ Laboratorios/Lab08Digital1Gar19085/Ejercicio02/ROM_tb.v
@@ -3,7 +3,7 @@
 module testbench();
 
 reg [7:0]address;
-wire [7:0]data_out;
+wire [7:0] data_out;
 
 
 ROM4Kx8 Ju87(address, data_out);
@@ -12,19 +12,25 @@ initial begin
         $display("\n");
         $display("Ejercicio 2");
         $display("address  |  data_out");
-        $monitor("%b", address, data_out);
+        $monitor("%b   |   %b", address, data_out);
         address = 0;
         #1 address = 0;
-        #10 addres = 1;
+        #1 address = 1;
+        #1 address = 2;
+        #1 address = 3;
+        #1 address = 4;
+        #1 address = 5;
+        #1 address = 6;
+        #1 address = 7;
 end
 
-    initial 
-        #20 $finish;
+//   initial 
+//        #20 $finish;
 
-        initial begin
-            $dumpfile("ROM_tb.vcd");
-            $dumpvars(0, testbench);
-        end
+//        initial begin
+//            $dumpfile("ROM_tb.vcd");
+//            $dumpvars(0, testbench);
+//        end
 
 
 endmodule
