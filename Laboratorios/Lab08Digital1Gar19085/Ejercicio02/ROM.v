063577ae5d63e4fafa47de64ac3b00bbd3911a34
gar19085 <68084823+gar19085@users.noreply.github.com>
2020-10-23 21:32:11 -0600

    Lab08 V2.0

--- Laboratorios/Lab08Digital1Gar19085/Ejercicio02/ROM.v
+++ Laboratorios/Lab08Digital1Gar19085/Ejercicio02/ROM.v
@@ -2,13 +2,13 @@
 //LAB08
 
 
-module  ROM4Kx8(input   [:]address
+module  ROM4Kx8(input   [7:0]address
                 output  [7:0]data_out);
 
     assign data_out = my_memory(address);
     reg [7:0]   my_memory[0:4095];
 
     initial begin
-        $redmemh("memory.list", ,y_memory);
+        $readmemh("memory.list", my_memory);
     end
 endmodule    
--- Laboratorios/Lab08Digital1Gar19085/Ejercicio02/ROM_tb.v
+++ Laboratorios/Lab08Digital1Gar19085/Ejercicio02/ROM_tb.v
@@ -2,18 +2,29 @@
 
 module testbench();
 
-reg ;
-wire ;
+reg [7:0]address;
+wire [7:0]data_out;
 
 
-ROM4Kx8 Ju87();
+ROM4Kx8 Ju87(address, data_out);
 
 initial begin
         $display("\n");
         $display("Ejercicio 2");
-        $display("CLK");
-        $monitor("%b", CLK);
+        $display("address  |  data_out");
+        $monitor("%b", address, data_out);
+        address = 0;
+        #1 address = 0;
+        #10 addres = 1;
 end
+
+    initial 
+        #20 $finish;
+
+        initial begin
+            $dumpfile("ROM_tb.vcd");
+            $dumpvars(0, testbench);
+        end
 
 
 endmodule
